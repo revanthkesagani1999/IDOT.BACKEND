b2fedb225b7ec1a5d10472ae1a5acc9157211250
revanthkesagani1999 <100779514+revanthkesagani1999@users.noreply.github.com>
2024-06-19 14:52:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static(path));
 
 var corsOptions = {
-  origin: ["http://localhost:4200"],
+  origin: ["http://localhost:4200","https://idot-ui-revanth1999s-projects.vercel.app"],
   credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
