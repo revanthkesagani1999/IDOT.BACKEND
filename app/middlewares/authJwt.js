fbb6cec8d3c63ea5d900ef06110a86bb920cb213
Revanth Kesagani <k.revanthgoud903@gmail.com>
2024-07-01 09:46:04 -0500

    token changes

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -4,9 +4,29 @@ const db = require("../models");
 const User = db.user;
 const Role = db.role;
 
+// verifyToken = (req, res, next) => {
+//   let token = req.session.token;
+//   //console.log("Token sent:",token);
+//   if (!token) {
+//     return res.status(403).send({ message: "No token provided!" });
+//   }
+
+//   jwt.verify(token, config.secret, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "Unauthorized!" });
+//     }
+//     req.userId = decoded.id;
+//     next();
+//   });
+// };
 verifyToken = (req, res, next) => {
-  let token = req.session.token;
-  //console.log("Token sent:",token);
+  // Extract the token from the Authorization header
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  const token = authHeader.split(' ')[1]; // Assuming the header is "Bearer token"
   if (!token) {
     return res.status(403).send({ message: "No token provided!" });
   }
