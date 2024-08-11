439e552fa8741576a01d165cf00bf6459ae8e75d
Revanth Kesagani <k.revanthgoud903@gmail.com>
2024-08-11 18:34:15 -0500

    multiple cpus

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
+const cluster = require('cluster');
 const http = require('http');
+const numCPUs = require('os').cpus().length; // Get the number of CPU cores
 const app = require('./app');
+
 const port = process.env.PORT || 8083;
 
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
 
-const server = http.createServer(app);
-server.listen(port,()=>{console.log('this app is running on '+port)});
+    // Fork workers for each CPU
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        console.log('Forking a new worker');
+        cluster.fork(); // Restarting the worker
+    });
+} else {
+    // Each worker creates an HTTP server
+    const server = http.createServer(app);
+
+    server.listen(port, () => {
+        console.log(`Worker ${process.pid} started, listening on ${port}`);
+    });
+}
